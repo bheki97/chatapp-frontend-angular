aaa299b4efdd59f2cd96383258f1a052e0a77400
bheki97 <bfmokoena7@gmail.com>
2023-12-22 11:40:23 +0200

    Changes made

---
+++ src/app/component/modal/search-geek/search-geek.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-geek',
+  templateUrl: './search-geek.component.html',
+  styleUrls: ['./search-geek.component.css']
+})
+export class SearchGeekComponent {
+
+}
